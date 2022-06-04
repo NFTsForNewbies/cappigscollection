9edd22d00ed39c9fa9f767ebf2f8e8fc73fda231
NFTsForNewbies <90934120+NFTsForNewbies@users.noreply.github.com>
2022-06-04 12:27:28 -0700

    Update collection.sol

--- collection.sol
+++ collection.sol
@@ -604,6 +604,8 @@ interface IERC721Receiver {
 
 // File: @openzeppelin/contracts/token/ERC721/ERC721.sol
 pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Context.sol";
 /**
  * @dev Implementation of https://eips.ethereum.org/EIPS/eip-721[ERC721] Non-Fungible Token Standard, including
  * the Metadata extension, but not including the Enumerable extension, which is available separately as
